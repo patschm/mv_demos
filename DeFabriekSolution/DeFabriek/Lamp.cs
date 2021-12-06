44a128189172a2d33271992ffe618e6c3117bb3b
Patrick Schmidt <pschmidt@pasit.nl>
2021-12-06 12:01:51 +0100

    first

---
+++ DeFabriekSolution/DeFabriek/Lamp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masevon
+{
+    // Blauwdruk van de lamp
+    // Type Definitie
+    class Lamp
+    {
+        // Fields. Hierin slaan we eigeschappen op.
+        private int _intensiteit = 100;
+
+        // Auto generating property. Die neemt zijn eige field mee
+        public ConsoleColor Kleur { get; set;}
+
+        // Properties. Hiermee geeft men gecontroleerde toegang tot fields
+        public int Intensiteit
+        {
+            get
+            {
+                return _intensiteit;
+            }
+            set
+            {
+                if (value >= 0 && value < 1000)
+                {
+                    _intensiteit = value;
+                }
+            }
+        }
+
+        // Gedrag leg je vast in Methods
+        public void Aan()
+        {
+            Console.BackgroundColor = Kleur;
+            Console.WriteLine($"De lamp gaat aan met een intensiteit van {Intensiteit} lumen");
+            Console.ResetColor();
+        }
+
+        // Constructors. Bedoeld om fields een initiele waarde te geven
+        public Lamp(int lumen)
+        {
+            Intensiteit = lumen;
+        }
+        public Lamp() : this(200)
+        {
+            
+        }
+    }
+}
---
+++ DeFabriekSolution/DeFabriek/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Masevon;
+
+namespace DeFabriek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Big bang
+            var lp = new Lamp(300);
+            //.intensiteit = -200;
+            //lp.SetIntensiteit(200);
+            lp.Kleur = ConsoleColor.Yellow;
+            lp.Intensiteit = 300;
+            lp.Aan();
+
+            var lp2 = new Lamp { Intensiteit = 700, Kleur = ConsoleColor.Red };
+            //lp2.SetIntensiteit(-500);
+            lp2.Aan();
+       
+
+
+
+
+
+            // Big crunch
+        }
+    }
+}
