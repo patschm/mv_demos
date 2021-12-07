e2881b6811f859f5f97407f813875632dc8767b7
Patrick Schmidt <pschmidt@pasit.nl>
2021-12-07 14:01:05 +0100

    d2o

--- Dag_2/WillemEnCo/SimonVDMeer.cs
+++ Dag_2/WillemEnCo/SimonVDMeer.cs
@@ -8,5 +8,13 @@ namespace WillemEnCo
 {
     class SimonVDMeer
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
--- Dag_2/WillemEnCo/WillemKlein.cs
+++ Dag_2/WillemEnCo/WillemKlein.cs
@@ -8,5 +8,13 @@ namespace WillemEnCo
 {
     class WillemKlein
     {
+        public void Rekenuit()
+        {
+            Console.WriteLine("Willem gaat rekenen");
+            int result = 0;
+
+
+            Console.WriteLine($"Willem is klaar en zegt dat het antwoord {result} is");
+        }
     }
 }
